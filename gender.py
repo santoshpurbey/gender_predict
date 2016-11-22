2786b0dde00976e34115aa267df9d28def86fcc2
santoshpurbey <purbey.santosh@hotmail.com>
2016-11-22 10:34:59 +0545

    ''

---
+++ gender.py
@@ -0,0 +1,39 @@
+male_name = ['hari','ramu' ]
+common_name = ['suman', 'sonu', 'komal', 'laxmi']
+female_name = ['i', 'u', 'a',
+               'ti','ka','ma','na','nu','ta','ti', 'tu', 'sa','ya',
+               'sha',
+               'ree',]
+
+
+def main():
+    name = raw_input('Enter your name:')
+    result = isfemale(name)
+    if result:
+        print 'Female'
+    elif result=='common name!':
+        print 'common name!'
+    else:
+        print 'Male'
+    
+def isfemale(name):
+    if (name in male_name):
+                return False
+    elif (name in common_name):
+                return 'common name!'
+    else:
+        for l in female_name:
+            if name.endswith(l):
+                return True
+                break
+        else:
+            return False
+    
+if __name__=='__main__':
+    while True:
+        main()
+        
+    
+
+        
+    
